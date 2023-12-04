8d6866dd4f4ea3b8c15032f1a1dbf97d56be25e7
Masud Parvez <thisismasud1@gmail.com>
2023-12-04 20:04:03 +0600

    update

--- index.js
+++ index.js
@@ -1,20 +1,59 @@
-//third party dependencies
-const mongoose = require("mongoose");
-const helmet = require("helmet");
-require("dotenv").config();
-const cors = require("cors");
-const morgan = require("morgan");
+/**
+ * Title: A Restful API for TODO application
+ * Description: This is the main file that is connecting to database and managing all the middlewares.
+ * Author: Masud Parvez
+ * Date: 2023-12-04
+ */
+
+//Basic Libraries
 const bodyParser = require("body-parser");
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
-//middlewares
+//security middleware libraries
+const helmet = require("helmet");
+const morgan = require("morgan");
+const { rateLimit } = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
+const cors = require("cors");
+
+//Database libraries
+const mongoose = require("mongoose");
+
+//application dependencies
+const router = require("./routes/route");
+
+//implemantation of middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // Parse JSON bodies
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-//helo word
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies (e.g., form data)
+app.use(mongoSanitize()); // By default, in mongosanitize$ and . characters are removed completely from user-supplied input in the following places: req.body, req.params, req.headers, req.query
+
+//User Request rate limit
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+});
+app.use(limiter);
+
+//mongoDB connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/Todo")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("err"));
+
+//Routing implementation
+app.use("/api/v1", router);
+
+//undefined routes implementation
+app.use("*", (req, res) => {
+  res.status(404).json("File not found");
+});
 
 //port
 const port = process.env.PORT || 3000;
--- routes/route.js
+++ routes/route.js
@@ -1,1 +1,16 @@
-console.log("hello");
+/**
+ * Title: A Restful API for TODO application
+ * Description: This is the router file. Handling all the routes and actions.
+ * Author: Masud Parvez
+ * Date: 2023-12-04
+ */
+
+//dependencies
+const express = require("express");
+const router = express.Router();
+
+router.get("/hello", (req, res) => {
+  res.json("Hello How are you");
+});
+//module exports
+module.exports = router;
